d48b8e6c9878c39fc4bf91d0a875f83f06bfe17e
dostourian <dostourian@gmail.com>
2026-01-23 20:50:35 -0500

    feat: csi-driver-smb by AD service account and AuthN by Kerberos

--- csi/csi-driver-smb/csi-driver-smb.sh
+++ csi/csi-driver-smb/csi-driver-smb.sh
@@ -47,28 +47,106 @@ prep(){
     }
 }
 
-mountCIFS(){ 
-    # For access to Windows share from a RHEL host:
-    echo "ℹ️ Mount a CIFS share from RHEL host ($(hostname -f))."
+setCreds(){
+    [[ $3 ]] || echo "  USAGE: $FUNCNAME user pass realm 2>&"
+    [[ $3 ]] || return 1
     
+	mkdir -p /etc/cifs || return 2
+	tee /etc/cifs/svc-smb-rw.creds <<-EOH
+	username=$1
+	password=$2
+	domain=$3
+	EOH
+      chmod 600 /etc/cifs/svc-smb-rw.creds
+}
+mountCIFS(){ 
+    [[ "$(id -u)" -ne 0 ]] && return 1
+    mode=${1:-service} # service|group|unmount
+
     # 1. Install CIFS (SMB) utilities
-    sudo dnf -y install cifs-utils ||
-        echo "❌ ERR : $?"
+    dnf list installed cifs-utils ||
+        dnf -y install cifs-utils ||
+            return 2
     
     # 2. Mount a Windows SMB share (regardless of its local filesystem format)
     realm=LIME
-    server=192.168.11.100
-    share=/NTFS002share
-    mnt=/mnt/100-ntfs002
-    ## Creds only if sec=sys : Utilizes legacy, insecure NTLMv2
-    user=CIFS_SVC_ACCT_USERNAME
-    pass=CIFS_SVC_ACCT_PASSWORD
+    server=dc1.lime.lan
+    share=SMBdata
+    mnt=/mnt/smb-data-01
+    svc=svc-smb-rw
+    ## Creds only if sec=ntlmssp 
+    creds=/etc/cifs/$svc.creds
+    mkdir -p $mnt || return 3
+    uid="$(id -u $svc)"
 
-    sudo mount -t cifs //$server/$share $mnt \
-        -o username=$user,password=$pass,vers=3.0,domain=$realm ||
-            echo "❌ ERR : $?"
+    [[ $mode == unmount ]] && {
+        umount $mnt
+        return $?
+    }
+    echo "ℹ️ Mount a CIFS share from RHEL host ($(hostname -f)) for '$mode' access."
+ 
+    # Allow R/W access by only AD User 'svc-smb-rw' 
+    gid="$(id -g svc-smb-rw)"
+    [[ $mode == service ]] && {
+        mount -t cifs //$server/$share $mnt \
+            -o sec=ntlmssp,credentials=$creds,vers=3.0,uid=$uid,gid=$gid,file_mode=0640,dir_mode=0775 ||
+                return 4
+    }
+    
+    # Allow R/W access by all members of AD Group 'ad-smb-admins'
+    gid="$(getent group ad-smb-admins |cut -d: -f3)"
+    [[ $mode == group ]] && {
+        mount -t cifs //$server/$share $mnt \
+            -o sec=ntlmssp,credentials=$creds,vers=3.0,uid=$uid,gid=$gid,file_mode=0660,dir_mode=0775 ||
+                return 5
+    }
+    
+    return 0
 }
 
-pushd "${BASH_SOURCE%/*}" || pushd . || return 1
-"$@" || echo "ERR: $?"
-popd
+mountCIFSkrb5(){
+    [[ "$(id -u)" -ne 0 ]] && return 1
+    mode=${1:-service} # service|group|unmount
+
+    realm=LIME
+    server=dc1.lime.lan
+    share=SMBdata
+    mnt=/mnt/smb-data-01
+    svc=svc-smb-rw
+    mkdir -p $mnt || return 3
+    uid="$(id -u $svc)"
+
+    [[ $mode == unmount ]] && {
+        umount $mnt
+        return $?
+    }
+    echo "ℹ️ Mount a CIFS share from RHEL host ($(hostname -f)) for '$mode' access using Kerberos for AuthN."
+ 
+    # Allow R/W access by only AD User 'svc-smb-rw' 
+    gid="$(id -g $svc)"
+    [[ $mode == service ]] && {
+        mount -t cifs //$server/$share $mnt \
+            -o sec=krb5,vers=3.0,cruid=$uid,uid=$uid,gid=$gid,file_mode=0640,dir_mode=0775 ||
+                return 4
+    }
+    
+    # Allow R/W access by all members of AD Group 'ad-smb-admins'
+    gid="$(getent group ad-smb-admins |cut -d: -f3)"
+    [[ $mode == group ]] && {
+        mount -t cifs //$server/$share $mnt \
+            -o sec=krb5,vers=3.0,cruid=$uid,uid=$uid,gid=$gid,file_mode=0660,dir_mode=0775 ||
+                return 5
+    }
+    
+    return 0
+
+}
+
+[[ $1 ]] || {
+    cat $BASH_SOURCE
+    exit
+}
+pushd "${BASH_SOURCE%/*}" >/dev/null 2>&1 || pushd . >/dev/null 2>&1 || return 1
+"$@" || echo "❌  ERR: $?"
+popd >/dev/null 2>&1
+
