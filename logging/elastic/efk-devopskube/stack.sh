4493cb60795a6cb2d66d66700269255bcd59f2d6
sempernow <dostourian@gmail.com>
2026-02-27 11:00:05 -0500

    chore: prune project down to cluster install/admin only

--- logging/elastic/efk-devopskube/stack.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# -----------------------------------------------------------------------------
-#  https://devopscube.com/setup-efk-stack-on-kubernetes/
-# -----------------------------------------------------------------------------
-
-type -f git || exit 1
-
-git clone https://github.com/scriptcamp/kubernetes-efk
--- security/oci/syft-grype-install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh |
-    sudo sh -s -- -b /usr/local/bin
-curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh |
-    sudo sh -s -- -b /usr/local/bin
